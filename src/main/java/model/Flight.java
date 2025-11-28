c9c7b3c0cb7079e795cc3ecfe63d0a61aa85db33
Umar Khan <muhammed.khan@ucalgary.ca>
2025-11-28 01:35:25 -0700

    add: adding flight class

---
+++ src/main/java/model/Flight.java
@@ -0,0 +1,12 @@
+package main.java.model;
+
+import java.util.Date;
+
+public class Flight {
+    String flightCode;
+    String airLine;
+    Date flightDate;
+    String flightDuration;
+    AreaCode departureAreaCode;
+    AreaCode arrivalAreaCode;
+}
