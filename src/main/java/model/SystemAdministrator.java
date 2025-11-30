b98fcd0ca075e006d8c04e196533b474994e2e6c
abdul-shakoorraed <144608983+abdul-shakoorraed@users.noreply.github.com>
2025-11-29 20:37:29 -0700

    fixed SystemAdmin

--- src/main/java/model/SystemAdministrator.java
+++ src/main/java/model/SystemAdministrator.java
@@ -1,7 +1,72 @@
 package main.java.model;
+import java.util.List;
+import main.java.service.FlightService;
+
+
 
 public class SystemAdministrator {
-    String username;
-    String password;
 
+    // Fields are set below
+    private boolean loggedIn = false;
+    private Customer loggedCustomer;
+    private FlightService flightService;
+
+    // Constructor is written here
+    public SystemAdministrator() {
+        flightService = new FlightService();
+    }
+    
+    // Login
+    public boolean login(String username, String password, List<Customer> customers) {
+        
+		// Looping through all of the customers here that are passed into this method here
+		for (Customer c : customers) {
+			if (c.username.equals(username) && c.password.equals(password)) {
+				// If match found -> login success
+				this.loggedIn = true;
+				this.loggedCustomer = c;
+				
+				System.out.println("Login is successful! You are Welcome, " + c.fname + " " + c.lname);
+				return true;
+		    }
+		}
+		
+		// If no match is found then do this
+		System.out.println("Invalid username or password.");
+		return false;
+	}
+	
+    // Admin Functions
+    public void addFlight(Flight flight) {
+        // calling flightService.addFlight(flight)
+		flightService.addFlight(flight);
+		// printing
+		System.out.println("Flight has been added successfully");
+    }
+
+    public void updateFlight(String flightID, Flight updatedFlight) {
+        // calling flightService.updateFlight(flightID, updatedFlight) here
+		flightService.updateFlight(flightID, updatedFlight); 
+		System.out.println("Flight has been updated.");
+    }
+
+    public void deleteFlight(String flightID) {
+        // calling flightService.deleteFlight(flightID) here
+		flightService.deleteFlight(flightID);
+			
+		System.out.println("Flight has been deleted successfully.");
+    }
+
+    public List<Flight> viewFlightSchedule() {
+        // getting the flights from the service below
+		List<Flight> schedule = flightService.getAllFlights();
+		
+		// printing each flight
+		for (Flight f : schedule) {
+			System.out.println(f.flightCode + " on " + f.flightDate);
+		}
+		return schedule;
+	} 
+     
+    
 }
--- src/main/java/service/FlightService.java
+++ src/main/java/service/FlightService.java
@@ -1,5 +1,73 @@
 package main.java.service;
 
+import main.java.model.Flight;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
 public class FlightService {
+
+    // The temporary in-memory list is written below.
+    private List<Flight> flights = new ArrayList<>();
     
+    public FlightService() {
+        // Constructor is here
+        // Initialize the DAO here when ready
+        // this.flightDAO = new FlightDAO();
+    }
+
+    // CREATE – Add a new flight
+
+    public void addFlight(Flight flight) {
+        // TEMPORARY: stores in memory
+        flights.add(flight);
+
+        // When DB is ready:
+        // flightDAO.insertFlight(flight);     // Uncomment these when needed 
+
+        System.out.println("FlightService: addFlight() is called for " + flight.flightCode);
+    }
+
+    // Updating – Updates an existing flight
+    public void updateFlight(String flightID, Flight updatedFlight) {
+        // There is the Temporary - in-memory update written here
+        for (int i = 0; i < flights.size(); i++) {
+            if (flights.get(i).flightID.equals(flightID)) {
+                flights.set(i, updatedFlight);
+                System.out.println("FlightService: flight has been updated (in-memory).");
+                return;
+            }
+        }
+
+        // When DB is ready:
+        // flightDAO.updateFlight(flightID, updatedFlight);
+
+        System.out.println("FlightService: updateFlight() is called for ID " + flightID);
+    }
+
+    
+    // Delete – Delete a flight by ID
+ 
+    public void deleteFlight(String flightID) {
+        // TEMPORARY in-memory delete here
+        flights.removeIf(f -> f.flightID.equals(flightID));
+
+        // When DB is ready:
+        // flightDAO.deleteFlight(flightID);
+
+        System.out.println("FlightService: deleteFlight() is called for ID " + flightID);
+    }
+
+    // Read – Get all flights (view flight schedule)
+    public List<Flight> getAllFlights() {
+        // Temporary return in-memory list
+
+        // When DB is ready:
+        // return flightDAO.getAllFlights();
+
+        return Collections.unmodifiableList(flights);
+    }
+
+    // Search methods can be added here later if needed
 }
