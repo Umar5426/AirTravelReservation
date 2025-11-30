ff968c2603cb63e86148f1e3118d771bbd0706d5
Muhammad Shaheer Shakir <144759340+Shaheer12345678@users.noreply.github.com>
2025-11-29 17:47:51 -0700

    Update Observer interface for event specificity
    Refactor Observer interface to specify MonthlyPromotionEvent.

--- src/main/java/model/Observer.java
+++ src/main/java/model/Observer.java
@@ -1,5 +1,11 @@
 package main.java.model;
 
-public interface Observer<E> {
-    void update(E event);
+/*
+ * Observer interface
+ * Any class that wants to "listen" for the monthly promotion event
+ * implements this interface.
+ */
+public interface Observer {
+    void update(MonthlyPromotionEvent event);
 }
+
