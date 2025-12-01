df792a282c2ed549f27ecf4ec7771d50e38b01b3
Umar Khan <muhammed.khan@ucalgary.ca>
2025-12-01 07:17:37 -0700

    add: function login

--- src/main/java/model/FlightAgent.java
+++ src/main/java/model/FlightAgent.java
@@ -4,31 +4,47 @@ import java.util.Date;
 import main.java.dao.CustomerDAO;
 
 public class FlightAgent extends Customer {
-    private String authorizedKey;
 
-    public FlightAgent(String fname, String lname, Date dob, String customerID, String username, String password, String authorizedKey) {
-        super(fname, lname, dob, customerID, username, password);
+    private String authorizedKey;
+    private CustomerDAO customerDAO;  // DAO instance
+
+    public FlightAgent(String fname, String lname, Date dob, String username, String password, String authorizedKey) {
+        // Pass null for customerID; CustomerDAO handles unique ID assignment
+        super(fname, lname, dob, null, username, password);
         this.authorizedKey = authorizedKey;
+        this.customerDAO = new CustomerDAO();
     }
 
-    public void addCustomer(String fname, String lname, Date dob, String customerID,
-                    String username, String password){
+    // -----------------------------
+    // Add a new customer
+    // -----------------------------
+    public boolean addCustomer(String fname, String lname, Date dob, String username, String password) {
+        // CustomerDAO will handle ID assignment
+        return customerDAO.addCustomer(fname, lname, dob, username, password);
+    }
 
-        CustomerDAO addCustomer(fname, lname, dob, customerID, username, password);
+    // -----------------------------
+    // View a customer by ID
+    // -----------------------------
+    public Customer viewCustomer(String customerID) {
+        return customerDAO.fetchById(customerID);
+    }
 
+    // -----------------------------
+    // Edit a customer
+    // -----------------------------
+    public boolean editCustomer(String customerID, String fname, String lname, Date dob, String password) {
+        return customerDAO.editCustomer(customerID, fname, lname, dob, password);
+    }
 
-    };
-
-    public Customer viewCustomer(String customerID){
-        Customer c.findbyId(customerID);
-    };
-
-    public void editCustomer(){};
-
+    // -----------------------------
+    // Setter for authorizedKey
+    // -----------------------------
     public void setAuthorizedKey(String authorizedKey) {
         this.authorizedKey = authorizedKey;
-
     }
 
-
+    public String getAuthorizedKey() {
+        return this.authorizedKey;
+    }
 }
