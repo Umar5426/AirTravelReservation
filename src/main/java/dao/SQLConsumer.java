a9b6461334245b07712f9581c49fe41182595e1c
Umar Khan <muhammed.khan@ucalgary.ca>
2025-12-01 07:46:21 -0700

    remove: un-needed files

--- src/main/java/dao/SQLConsumer.java
+++
@@ -1,9 +0,0 @@
-package main.java.dao;
-
-import java.sql.SQLException;
-
-// Functional interface to allow lambdas that throw SQLExceptions
-@FunctionalInterface
-public interface SQLConsumer<T> {
-    void accept(T t) throws SQLException;
-}
